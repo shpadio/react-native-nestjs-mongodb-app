ed53c7a1bfb184a1695a267991ec051d7d8171ce
Oleg Shpadin <73585953+shpadio@users.noreply.github.com>
2022-01-30 18:24:45 +0300

    modified gitignore

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}cluster0.t49xt.mongodb.net/${process.env.DB_NAME}`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
